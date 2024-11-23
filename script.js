14a87aeed1bae32a33fcfc7d8dfdcc1e0041e897
unknown <2002zainabaamir@gmail.com>
2024-11-22 23:48:49 -0500

    added another form for focus and blur feature, input box changes colours and a message appers

--- script.js
+++ script.js
@@ -56,15 +56,15 @@ function saveName (){
 function timeGreet() { 
     const time = new Date().getHours();
     if (time >= 5 && time <=10 ) { 
-        return 'Good Morning! Seize the Day and Make it Amazing!,  '; 
+        return 'Good Morning! Seize the Day and Make it Amazing,  '; 
     }
     
     else if (time >= 11 && time <= 17 ) { 
-        return 'Good Afternoon! Keep up the Great Work!, '; 
+        return 'Good Afternoon! Keep up the Great Work, '; 
     }
     
     else if (time >= 18 && time <= 22){ 
-        return 'Good Evening! Time to Unwind and Relax!, ';
+        return 'Good Evening! Time to Unwind and Relax, ';
     }
     
     else {
@@ -73,3 +73,18 @@ function timeGreet() {
     }
     
     updateDisplay();
+
+
+
+    const inputField = document.getElementById('focusBlur');
+    const messageF = document.getElementById('focusMessage');
+
+    function handleFocus() {
+        document.getElementById('focusBlur').style.background = 'violet';
+        messageF.textContent = "You Found Me!";
+    }
+
+    function handleBlur() {
+        document.getElementById('focusBlur').style.background = 'purple';
+        messageF.textContent = "I'm lost!";
+    }
