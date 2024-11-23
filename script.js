00beb580dd720613d8f4ba0ef8028232efc403d9
unknown <2002zainabaamir@gmail.com>
2024-11-22 20:31:25 -0500

    added a feature that when a ket is press a message informs user on which key was pressed

--- script.js
+++ script.js
@@ -7,12 +7,19 @@ buttonH.addEventListener('mouseleave', () => {
     buttonH.textContent = "I miss you come back!";
 });
 
+
+
 const buttonC = document.getElementById('clickButton');
 
 buttonC.addEventListener('mousedown', () => {
-    buttonC.textContent = "Ouch that's to hard!";
+    buttonC.textContent = "Ouch that's too hard!";
 });
 buttonC.addEventListener('mouseup', () => {
     buttonC.textContent = "That's better";
 });
 
+const messageK = document.getElementById('keyMessage');
+
+document.addEventListener('keydown', function(event) {
+    messageK.textContent = (`you have pressed the: ${event.key} key on your keyboard`);
+});
