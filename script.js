7bcb9bc37f1bace6af91ea7ae1b946c2215eeb00
unknown <2002zainabaamir@gmail.com>
2024-11-22 19:58:10 -0500

    added a second button that changes messages when clicked, first button changes messages when hovered.

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-const button = document.getElementById('clickButton');
+const buttonH = document.getElementById('hoverButton');
 
-button.addEventListener('mouseenter', () => {
-    button.textContent = "Hey that tickles!";
+buttonH.addEventListener('mouseenter', () => {
+    buttonH.textContent = "Hey that tickles!";
+});
+buttonH.addEventListener('mouseleave', () => {
+    buttonH.textContent = "I miss you come back!";
 });
 
-button.addEventListener('mouseleave', () => {
-    button.textContent = "I miss you come back!";
+const buttonC = document.getElementById('clickButton');
+
+buttonC.addEventListener('mousedown', () => {
+    buttonC.textContent = "Ouch that's to hard!";
 });
+buttonC.addEventListener('mouseup', () => {
+    buttonC.textContent = "That's better";
+});
+
